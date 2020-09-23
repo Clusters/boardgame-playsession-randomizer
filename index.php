ae5d6272fc2ae8fd5bda605166ab1aca3c4414e4
Andreas <cluster1@gmx.at>
2020-09-23 22:17:03 +0200

    [Init] Debug environment created

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("./boardgame_thursday_loader.php");
+
+init();
+
+?>
+<html>
+    <header>
+        <title><?php echo '$title' ?></title>
+    </header>
+    <body>
+        <?php echo '$content' ?>
+        
+    </body>
+</html>
